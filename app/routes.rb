3447736a54905599002aad0c390910005aa2cd45
maximkuzmin <photographer.max@gmail.com>
2018-03-11 22:52:51 +0700

    fix routes with new controller names

--- app/routes.rb
+++ app/routes.rb
@@ -3,7 +3,7 @@
 # Maps main apps to proper routes
 class Routes
   def self.call(rack)
-    rack.map('/telegram') { run TelegramApp }
-    rack.map('/') { run Welcome }
+    rack.map('/telegram') { run TelegramController }
+    rack.map('/') { run WelcomeController }
   end
 end
